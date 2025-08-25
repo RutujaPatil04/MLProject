5d37200794f99cc4fbb382bc2f92e16d62a23281
RutujaPatil04 <rutuspatil7@gmail.com>
2025-08-25 13:22:26 +0530

    EDA and prblm stmt

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys #any exception handling sys will come inot picture
-import logging
+
+from logger import logging
 # How message will look like
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,3 +21,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e,sys)
+        
