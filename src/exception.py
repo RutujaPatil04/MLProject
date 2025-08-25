d2c92b6f8c32555fc0821256ffd053ecb94bcab8
RutujaPatil04 <rutuspatil7@gmail.com>
2025-08-25 19:03:09 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys #any exception handling sys will come inot picture
 
-from logger import logging
+from src.logger import logging
 # How message will look like
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
