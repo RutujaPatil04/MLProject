8df776d8fc82bd182769bb7ebb5f864545e9684d
RutujaPatil04 <rutuspatil7@gmail.com>
2025-08-28 22:54:59 +0530

    Predictor pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+import pickle
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -28,4 +29,12 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
         return report
      except Exception as e:
           raise CustomException(e,sys)
+     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
     
